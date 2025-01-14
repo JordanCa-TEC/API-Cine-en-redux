fa69816188052a0aae1c4bb2da28d8ed07c43b94
email <jordanmorel.ca@gmail.com>
2025-01-13 22:49:27 -0500

    movie modificacion

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const MovieCard = ({ movie }) => {
-  const { title, description, posterUrl } = movie;
+  const { title, overview, poster_path } = movie;
+
+  const posterUrl = `https://image.tmdb.org/t/p/w500${poster_path}`;
 
   return (
     <div className="movie-card">
       <img src={posterUrl} alt={`${title} poster`} className="movie-poster" />
       <div className="movie-details">
         <h2 className="movie-title">{title}</h2>
-        <p className="movie-description">{description}</p>
+        <p className="movie-description">{overview}</p>
       </div>
     </div>
   );
 };
 
-// Definición de PropTypes
-import PropTypes from 'prop-types';
-
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    posterUrl: PropTypes.string.isRequired,
+    overview: PropTypes.string.isRequired,
+    poster_path: PropTypes.string.isRequired,
   }).isRequired,
 };
-
 
 export default MovieCard;
